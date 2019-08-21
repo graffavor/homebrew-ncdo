5aa915ebd053b4db5f35337d4541f3a25668e63f
Andrey Stroganov <graffavor@gmail.com>
2019-08-22 01:11:14 +0700

    fix hash

--- Formula/ncdo.rb
+++ Formula/ncdo.rb
@@ -2,7 +2,7 @@ class Ncdo < Formula
   desc "Ncurses tool for managing todo.txt files"
   homepage "https://github.com/graffavor/ncdo"
   url "https://github.com/graffavor/ncdo/archive/0.0.2.zip"
-  sha256 "cb6bb70806d0921e68dd5bcf75c3a48c2db1b0b2"
+  sha256 "a2cb8a15d7014f62fb930869c4eaf2ab7aef73f7ca85a133f34f0770f2cc096d"
   head "https://github.com/graffavor/ncdo.git"
 
   depends_on "cmake" => :build
